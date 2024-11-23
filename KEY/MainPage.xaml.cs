27770314bd4e4472910d9cfa72ceab6742751d3d
Karen <Karen@DESKTOP-8FSV72H>
2024-11-22 20:14:16 -0600

    Agregar archivos de proyecto.

---
+++ KEY/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace KEY
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Placeholder para el botón de Google
+        private void OnGoogleButtonClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Google", "Función de Google en desarrollo.", "OK");
+        }
+
+        // Placeholder para el botón de Número de Teléfono
+        private void OnPhoneButtonClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Número de Teléfono", "Función de número de teléfono en desarrollo.", "OK");
+        }
+    }
+}
+
+
+
